54e7da35d8dfb303f28f667dceb698509c2d1970
MORGANE FLAUDER <morgane-externe.flauder@enedis.fr>
2017-04-27 09:14:18 +0200

    Chapter one - tutorial 05

---
+++ src/main/scala/com/allaboutscala/chapter/one/tutorial_05/HelloWorldMain.scala
@@ -0,0 +1,14 @@
+package com.allaboutscala.chapter.one.tutorial_05
+
+/**
+  * Created by Morgane Flauder on 27/04/2017.
+  */
+object HelloWorldMain {
+
+  def main(args: Array[String]): Unit = {
+
+    println("Hello World from main function!")
+
+  }
+
+}
