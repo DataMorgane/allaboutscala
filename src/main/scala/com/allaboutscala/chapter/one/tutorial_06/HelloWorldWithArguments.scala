4ce54ad56af159a94e58328976c56d653aba48c7
MORGANE FLAUDER <morgane-externe.flauder@enedis.fr>
2017-04-27 09:25:00 +0200

    Chapter one - tutorial 06

---
+++ src/main/scala/com/allaboutscala/chapter/one/tutorial_06/HelloWorldWithArguments.scala
@@ -0,0 +1,12 @@
+package com.allaboutscala.chapter.one.tutorial_06
+
+/**
+  * Created by Morgane Flauder on 27/04/2017.
+  */
+object HelloWorldWithArguments extends App {
+
+  println("Hello World with arguments Scala application!")
+  println("Command line arguments are:")
+  println(args.mkString(", "))
+
+}
