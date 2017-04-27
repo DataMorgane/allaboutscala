d27b818da78f02938a728f37b0a6e1e1987e5728
MORGANE FLAUDER <morgane-externe.flauder@enedis.fr>
2017-04-27 08:57:07 +0200

    Chapter one - tutorial 04

---
+++ src/main/scala/com/allaboutscala/chapter/one/tutorial_04/HelloWorld.scala
@@ -0,0 +1,10 @@
+package com.allaboutscala.chapter.one.tutorial_04
+
+/**
+  * Created by Morgane Flauder on 27/04/2017.
+  */
+object HelloWorld extends App {
+
+  println("Hello World from Scala!")
+
+}
