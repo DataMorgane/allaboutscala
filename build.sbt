7b14e0a37a9cfb1a2eb6c7a81033833dca2ab273
DataMorgane <morgane.flauder@gmail.com>
2017-04-28 14:32:06 +0200

    Chapter three - tutorial 01 - CreateAndUseFunction

--- build.sbt
+++ build.sbt
@@ -4,8 +4,13 @@ version := "1.0"
 
 scalaVersion := "2.12.2"
 
+val scalaV = "2.12.2"
+
 libraryDependencies ++= Seq (
   "com.typesafe.scala-logging" %% "scala-logging" % "3.5.0",
-  "ch.qos.logback" % "logback-classic" % "1.1.7"
+  "ch.qos.logback" % "logback-classic" % "1.1.7",
+  "org.scala-lang"    %   "scala-compiler"      % scalaV,
+  "org.scala-lang"    %   "scala-library"       % scalaV,
+  "org.scala-lang"    %   "scala-reflect"       % scalaV
 )
         
--- src/main/scala/com/allaboutscala/chapter/three/CreateAndUseFunction_01.scala
+++ src/main/scala/com/allaboutscala/chapter/three/CreateAndUseFunction_01.scala
@@ -5,6 +5,27 @@ package com.allaboutscala.chapter.three
   */
 object CreateAndUseFunction_01 extends App {
 
+  // 1. How to define and use a function which has a return type
+  def favoriteDonut(): String = {
+    "Glazed Donut"  // The last line is the returned value (no return keyword)
+  }
+  val myFavoriteDonut = favoriteDonut()
+  println(s"My favorite donut is $myFavoriteDonut")
 
+  // 2. How to define and use a function with no parenthesis
+  def leastFavoriteDonut = "Plain Donut"
+  println(s"My least favorite donut is $leastFavoriteDonut")
+
+  // 3. How to define and use a function with no return type
+  def printDonutSalesReport(): Unit = {
+    println("Printing donut sales report... done!")
+  }
+  printDonutSalesReport()
+
+  // 4. Use type inference to define function with no return type
+  def printReport: Unit = {
+    println("Printing donut report... done!")
+  }
+  printReport
 
 }
